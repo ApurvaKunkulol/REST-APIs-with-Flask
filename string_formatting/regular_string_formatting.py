ca21a89a5545932085e467b4626e8dd0160420b0
Apurva Kunkulol <kunkulol.apurva@gmail.com>
2020-07-02 20:34:43 +0530

    Started course

---
+++ string_formatting/regular_string_formatting.py
@@ -0,0 +1,11 @@
+__author__ = 'Apurva A Kunkulol'
+
+name = "Apurva"
+greeting = "Hello {}"
+with_name = greeting.format(name)
+
+print(with_name)
+
+longer_phrase = "Hi {}, today is {}!"
+
+print(longer_phrase.format(name, "Thursday"))
